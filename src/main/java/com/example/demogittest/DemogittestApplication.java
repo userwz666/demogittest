8b9a917477ebaae57fa21e5ed2ae463439424f56
GuanYi-Start <1119132301@qq.com>
2020-03-16 21:17:48 +0800

    zs01

---
+++ src/main/java/com/example/demogittest/DemogittestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogittestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demogittest/test.java
@@ -0,0 +1,8 @@
+package com.example.demogittest;
+
+public class test {
+
+    public static void main(String[] args){
+        System.out.println("11111111");
+    }
+}
